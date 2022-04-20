4fd0ffb423d0d604f1e36e01b174bba2321d975e
Jake Ritter <jakeritter77@gmail.com>
2022-04-19 23:50:55 -0700

    -initial commit

---
+++ create_table.php
@@ -0,0 +1,47 @@
+<?php
+
+$username = "jr550";
+$password = "Wo0dabu9a";
+
+
+$db_conn_str =
+            "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)
+                                       (HOST = cedar.humboldt.edu)
+                                       (PORT = 1521))
+                            (CONNECT_DATA = (SID = STUDENT)))";
+
+$conn = oci_connect($username, $password, $db_conn_str);
+
+if (!$conn) {
+    echo 'Error connecting...';
+}
+
+
+$create_table_str = "
+drop table example
+";
+
+$create_table_stmt = oci_parse($conn, $create_table_str);
+
+oci_execute($create_table_stmt, OCI_COMMIT_ON_SUCCESS);
+
+oci_free_statement($create_table_stmt);
+
+$create_table_str = "
+    create table example(
+        submission varchar(30)
+    )
+";
+
+$create_table_stmt = oci_parse($conn, $create_table_str);
+
+oci_execute($create_table_stmt, OCI_COMMIT_ON_SUCCESS);
+
+oci_free_statement($create_table_stmt);
+
+echo 'Table Created!';
+
+oci_close($conn);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>OCI Example</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+
+    <h1>OCI Example</h1>
+
+    <form>
+
+    <label>
+        Enter a any string:
+        <input type="text" id="my_str" required />
+    </label>
+    <input type="submit" id="submit_btn" value="Submit" />
+
+    </form>
+
+
+  </body>
+</html>
